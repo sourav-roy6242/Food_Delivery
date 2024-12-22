9e19d04d7c7970c20bc3e9c9c4723e1f5fdd26a9
sourav-roy6242 <roys45545@gmail.com>
2024-12-22 23:28:59 +0530

    url modified

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
 
   const [cartItems,setCartItems] = useState({});
 
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-backend-snug.onrender.com/"
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([])
 
