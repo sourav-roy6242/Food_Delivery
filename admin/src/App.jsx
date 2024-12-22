43a0b86cf4b826c21d8714b2d89e0639cef3b7a7
sourav-roy6242 <roys45545@gmail.com>
2024-12-22 23:34:04 +0530

    url modified

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://food-delivery-backend-snug.onrender.com/"
+  const url = "https://food-delivery-backend-snug.onrender.com"
 
   return (
     <div>
--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-snug.onrender.com/'
+export const url = 'https://food-delivery-backend-snug.onrender.com'
